6f96fb05ce4506f837825fe4c82883065bc19308
yuukichen <168069994+yuukichen@users.noreply.github.com>
2025-07-13 17:21:54 +0800

    Update pose_val.py

--- pose_val.py
+++ pose_val.py
@@ -7,6 +7,13 @@ from ultralytics import YOLO
 from ultralytics.utils.torch_utils import model_info
 
 
+# BILIBILI UP 魔傀面具
+# 验证参数官方详解链接：https://docs.ultralytics.com/modes/val/#usage-examples:~:text=of%20each%20category-,Arguments%20for%20YOLO%20Model%20Validation,-When%20validating%20YOLO
+
+# 精度小数点保留位数修改问题可看<使用说明.md>下方的<YOLOV8源码常见疑问解答小课堂>第五点
+# 最终论文的参数量和计算量统一以这个脚本运行出来的为准
+
+
 """
 关键点检测评估指标说明：
 
